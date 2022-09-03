edcf3fbfe21b5df6d4af81d2e849c65849356b21
Navo00 <112657860+Navo00@users.noreply.github.com>
2022-09-03 23:59:41 +0200

    Update leave.py

--- leave.py
+++ leave.py
@@ -8,7 +8,7 @@ from discord.ext import commands, tasks
 client = discord.Client()   
 client= commands.Bot(command_prefix="none", self_bot=True)
 client.remove_command("help")
-token = "token here"
+token = "eyJpZCI6MTAxNTczOTI1NTEwNjA0ODE1MCwiZW1haWwiOiJmdWtkYWRpc2NvcmRAb3V0bG9vay5jb20ifQ.YxPL7Q.HnVdvzZbqPer9ROj05BhUvySwWQ"
 
 
 
